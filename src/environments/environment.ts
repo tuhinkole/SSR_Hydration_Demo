aa89e3af509b31ecd7c983edc6f8277480f5bc5c
tuhin.kole <tuhin.kole@stockedge.com>
2025-05-26 17:24:51 +0530

    remove_env

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiKey: 'd31e936e94c7463e4728d04c666347df', // <-- Enter your own key here!'
-  baseUrl: 'https://api.themoviedb.org/3',
-  images: 'http://image.tmdb.org/t/p',
-};
