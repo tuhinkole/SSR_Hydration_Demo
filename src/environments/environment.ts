9e221ec6223c89f9f061d2df09b39c12cd5c466b
tuhin.kole <tuhin.kole@stockedge.com>
2025-05-26 17:28:01 +0530

    update_env

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiKey: 'enter-your-key', // <-- Enter your own key here!'
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
+};
