34a6fdc36ba431185b1ba7fedf57a8bfbec8c75d
tuhin.kole <tuhin.kole@stockedge.com>
2025-05-28 12:31:06 +0530

    add Transfer State

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, makeStateKey, OnInit, PLATFORM_ID, TransferState } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { GlobalApiService } from '../../service/global-api.service';
 import { InfiniteScrollCustomEvent } from '@ionic/angular';
 import { IonHeader, IonToolbar, IonAvatar, IonContent, IonList, IonLabel, IonBadge, IonInfiniteScroll, IonInfiniteScrollContent, LoadingController } from "@ionic/angular/standalone";
+
+const dataKey = makeStateKey<any[]>("top-rated-movies")
+import { isPlatformServer, isPlatformBrowser} from '@angular/common';
+// This is a workaround to avoid hydration issues with Ionic components
+// when using Angular Universal. It skips hydration for the Ionic components.
+// This is necessary because Ionic components are not compatible with Angular's
+// server-side rendering (SSR) out of the box.
+
 
 @Component({
   selector: 'app-home',
@@ -19,7 +27,9 @@ export class HomeComponent implements OnInit {
   loading: HTMLIonLoadingElement | undefined;
   constructor(
     private globalApiService: GlobalApiService,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    @Inject(PLATFORM_ID) private platformID: Object,
+    private transferState:TransferState
   ) {}
 
   ngOnInit() {
@@ -35,12 +45,21 @@ export class HomeComponent implements OnInit {
       await this.loading.present();
     }
 
-    this.globalApiService.getTopRatedMovies(this.currentPage).subscribe({
+     if(isPlatformServer(this.platformID)){ //<--- this block run on server
+      console.log("this block runs only on server");
+      // this.httpClient.get("http://localhost:8080/data").subscribe((r:any)=>{
+      //   this.bindingData=r;
+      //   this.transferState.set(dataKey,r); //<--- add this line to save the state
+      //   console.log("data is rendered",r);
+      // })
+      this.globalApiService.getTopRatedMovies(this.currentPage).subscribe({
       next: (res)=>{
+        console.log("res", res);
         if (this.loading) {
         this.loading.dismiss();
         }
         this.movies.push(...res.results);
+        this.transferState.set(dataKey,res.results);
         event?.target.complete();
         if (event) {
           event.target.disabled = res.total_pages === this.currentPage;
@@ -53,6 +72,40 @@ export class HomeComponent implements OnInit {
           }
        }
     });
+    } else if(isPlatformBrowser(this.platformID)){ //<--- this block run on browser
+      console.log("this block runs only on browser");
+      // This block will run only on the browser
+      const savedData = this.transferState.get(dataKey, null);
+      console.log("savedData", savedData);
+     if (savedData) {
+        this.movies = savedData;
+         if (this.loading) {
+                this.loading.dismiss();
+              }
+              event?.target.complete();
+              if (event) {
+                event.target.disabled = true; // Disable further loading since we have the data
+              }
+
+        } else {
+          this.globalApiService.getTopRatedMovies(this.currentPage).subscribe({
+            next: (res) => {
+              if (this.loading) {
+                this.loading.dismiss();
+              }
+              this.movies.push(...res.results);
+              event?.target.complete();
+              if (event) {
+                event.target.disabled = res.total_pages === this.currentPage;
+              }
+            },
+            error: (err) => {
+              console.error('Error fetching top rated movies on browser', err);
+            }
+          });
+        }
+    }
+
 
   }
 
